bedf6ca693ae6d6663902fd04245f021a840c46d
Smoukus <smoukus@mail.ru>
2024-02-27 18:40:00 +0300

    Добавление базовых файлов в проект

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+
+    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    
+    browser.implicitly_wait(5)
+    yield browser
+    browser.quit()
