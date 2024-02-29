2548f65efd09527cc2a5584ae3a519e7da0bda02
Smoukus <smoukus@mail.ru>
2024-02-29 14:14:34 +0300

    Удалил лишний импорт

--- conftest.py
+++ conftest.py
@@ -1,6 +1,5 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
