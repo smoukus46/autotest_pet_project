72508333081ef63c36723b8a747e5c8909f1f209
EVGENIYSTRUKOV\struk <strukov0504@inbox.ru>
2024-03-21 00:49:40 +0300

    Небольшая корректировка

--- pages/accordian_page.py
+++ pages/accordian_page.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from pages.base_page import BasePage
 from pages.main_page import MainPage, MainPageLocators
 
 texts_in_body_sections = ["""Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.""",
