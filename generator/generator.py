eacb6b2845d9116902d6c424cd066a3c83f69c6c
“Zheninja11” <strukov0504@inbox.ru>
2024-03-21 19:29:44 +0300

    Добавил генератор для даты

--- generator/generator.py
+++ generator/generator.py
@@ -4,7 +4,8 @@ import random
 
 
 Faker.seed()
-fake = Faker('ru_RU')
+fake_ru = Faker('ru_RU')
+fake_en = Faker('EN')
 
 @dataclass
 class Person:
@@ -25,19 +26,19 @@ class Person:
     - phone = Номер телефона.
     """
 
-    full_name: str = field(default_factory=fake.name)
-    email: str = field(default_factory=fake.email)
-    current_address: str = field(default_factory=fake.address)
-    permanent_address: str = field(default_factory=fake.address)
+    full_name: str = field(default_factory=fake_ru.name)
+    email: str = field(default_factory=fake_ru.email)
+    current_address: str = field(default_factory=fake_ru.address)
+    permanent_address: str = field(default_factory=fake_ru.address)
 
-    first_name: str = field(default_factory=fake.first_name)
-    last_name: str = field(default_factory=fake.last_name)
-    age: int = field(default_factory=lambda: fake.random_int(1, 100))
-    salary: int = field(default_factory=lambda: fake.random_int(20000, 100000))
-    department: str = field(default_factory=fake.word)
+    first_name: str = field(default_factory=fake_ru.first_name)
+    last_name: str = field(default_factory=fake_ru.last_name)
+    age: int = field(default_factory=lambda: fake_ru.random_int(1, 100))
+    salary: int = field(default_factory=lambda: fake_ru.random_int(20000, 100000))
+    department: str = field(default_factory=fake_ru.word)
 
-    subject: str = field(default_factory=fake.word)
-    phone: str = field(default_factory=fake.phone_number)
+    subject: str = field(default_factory=fake_ru.word)
+    phone: str = field(default_factory=fake_ru.phone_number)
 
 colors = ["Red", "Blue", "Green", "Yellow", "Purple", "Black", "White", "Voilet", "Indigo", "Magenta", "Aqua"]
 
@@ -45,3 +46,17 @@ colors = ["Red", "Blue", "Green", "Yellow", "Purple", "Black", "White", "Voilet"
 class Colors:
     """Выбирает случайный цвет из массива"""
     colors: str = field(default_factory=lambda: random.choice(colors))
+
+@dataclass
+class Date:
+    """Генерирует даты
+
+    - year = Год;
+    - month = Месяц;
+    - day = День месяца;
+    - time = Время;
+    """
+    year: str = field(default_factory=fake_en.year)
+    month: str = field(default_factory=fake_en.month)
+    day: str = field(default_factory=fake_en.day_of_month)
+    time: str = field(default_factory=fake_en.time)
