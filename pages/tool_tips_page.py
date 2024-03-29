b2bd2fa43d04b48cb494740e04f0714f6cef0b88
“Zheninja11” <strukov0504@inbox.ru>
2024-03-29 18:13:31 +0300

    Добавил локаторы

--- pages/tool_tips_page.py
+++ pages/tool_tips_page.py
@@ -10,4 +10,21 @@ class ToolTipsPage:
     # Кнопка Hover
     HOVER_BUTTON = (By.ID, "toolTipButton")
 
+    # Поле Hover me to see
+    FIELD_HOVER_ME_TO_SEE = (By.ID, "toolTipTextField")
+
+    # Надпись Contrary
+    INSCRIPTION_FIRST = (By.XPATH, "//a[text()='Contrary']")
+
+    # Ховер надписи Contrary
+    FIRST_TEXT_HOVER = (By.XPATH, "//div[text()='You hovered over the Contrary']")
+
+    # Надпись 1.10.32
+    INSCRIPTION_SECOND = (By.XPATH, "//a[text()='1.10.32']")
+
+    # Ховер надписи 1.10.32
+    SECOND_TEXT_HOVER = (By.XPATH, "//div[text()='You hovered over the 1.10.32']")
+
+
+class ToolTipsPage(MainPage):
     
