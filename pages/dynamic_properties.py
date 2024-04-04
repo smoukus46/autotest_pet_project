630a962a8635183f916feba30eda00b215fb7ac3
Smoukus <smoukus@mail.ru>
2024-04-04 17:31:00 +0300

    Отлажен тест для страницы Dynamic Properties

--- pages/dynamic_properties.py
+++ pages/dynamic_properties.py
@@ -21,12 +21,13 @@ class DynamicPropertiesPage(MainPage):
     def color_changing_button_white(self):
         """Проверяет кнопку с изменяющимся цветом текста на белый цвет"""
         color = self.find_element(DynamicPropertiesPageLocators.COLOR_CHANGE_BUTTON).value_of_css_property('color')
-        assert color == '#fff', 'Цвет не соответствует ожидаемому'
+        print(color)
+        assert color == 'rgba(255, 255, 255, 1)', 'Цвет не соответствует ожидаемому'
 
     def color_changing_button_red(self):
         """Проверяет кнопку с изменяющимся цветом текста на смену цвет на красный"""
         color = self.find_element(DynamicPropertiesPageLocators.COLOR_CHANGE_BUTTON).value_of_css_property('color')
-        assert color == 'dc3545', 'Цвет не соответствует ожидаемому'
+        assert color == 'rgba(220, 53, 69, 1)', 'Цвет не соответствует ожидаемому'
 
     def visible_after_five_seconds_button_is_displayed(self):
         """Проверяет отображение кнопки на странице после истечения пяти секунд"""
--- tests/test_dynamic_properties_page.py
+++ tests/test_dynamic_properties_page.py
@@ -12,9 +12,9 @@ def test_check_dynamic_properties(browser):
     dynamic_properties_page.elements_menu_items_is_displayed()
     dynamic_properties_page.open_dynamic_properties_tab()
     dynamic_properties_page.will_enable_button_is_disabled()
-    dynamic_properties_page.visible_after_five_seconds_button_is_not_displayed()
     dynamic_properties_page.color_changing_button_white()
+    dynamic_properties_page.visible_after_five_seconds_button_is_not_displayed()
     time.sleep(6)
-    assert 'None' == dynamic_properties_page.will_enable_button_is_disabled(), 'Кнопка неактивна'
+    assert None is dynamic_properties_page.will_enable_button_is_disabled(), 'Кнопка неактивна'
     dynamic_properties_page.color_changing_button_red()
     dynamic_properties_page.visible_after_five_seconds_button_is_displayed()
