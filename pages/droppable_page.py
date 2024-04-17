4fcd9b4e09c71c65854b183d7ab634227b0ed9ed
EVGENIYSTRUKOV\struk <strukov0504@inbox.ru>
2024-04-17 23:25:54 +0300

    Дописал локаторы и методы для вкладок Prevent Propogation и Rever Draggable, дописал тело теста для этих вкладок

--- pages/droppable_page.py
+++ pages/droppable_page.py
@@ -12,7 +12,7 @@ class DroppablePageLocators:
     # Кнопка вкладки Simple
     SIMPLE_TAB = (By.XPATH, "//a[@id='droppableExample-tab-simple']")
 
-    # Элемент Drage me вкладки Simple
+    # Элемент Drag me вкладки Simple
     SIMPLE_TAB_DRAG_ME = (By.XPATH, "//div[@class='simple-drop-container']/div[1]")
 
     # Элемент Drop box вкладки Simple
@@ -36,6 +36,38 @@ class DroppablePageLocators:
     # Текст элемента drop box вкладки Accept
     ACCEPT_TAB_DROP_BOX_TEXT = (By.XPATH, "//div[@class='accept-drop-container']/div[2]/p")
 
+    # Кнопка вкладки Prevent Propogation
+    PREVENT_PROPOGATION_TAB = (By.XPATH, "//a[@id='droppableExample-tab-preventPropogation']")
+
+    # Элемент Drag me вкладки Prevent Propogation
+    PREVENT_PROPOGATION_DRAG_ME = (By.XPATH, "//div[@class='drag-box mt-4 ui-draggable ui-draggable-handle' and @id='dragBox']")
+
+    # Элемент Not greedy inner вкладки Prevent Propogation
+    NOT_GREEDY_INNER_DROP_BOX = (By.XPATH, "//div[@id='notGreedyDropBox']/div/p")
+
+    # Элемент Not greedy outer вкладки Prevent Propogation
+    NOT_GREEDY_OUTER_DROP_BOX = (By.XPATH, "//div[@id='notGreedyDropBox']/p")
+
+    # Элемент Greedy inner вкладки Prevent Propogation
+    GREEDY_INNER_DROP_BOX = (By.XPATH, "//div[@id='greedyDropBoxInner']/p")
+
+    # Элемент Greedy outer вкладки Prevent Propogation
+    GREEDY_OUTER_DROP_BOX = (By.XPATH, "//div[@id='greedyDropBox']/p")
+
+    # Кнопка вкладки Revert Draggable
+    REVERT_DRAGGABLE_TAB = (By.XPATH, "//a[@id='droppableExample-tab-revertable']")
+
+    # Элемент Will Revert вкладки Revert Draggable
+    REVERT_DRAGGABLE_WILL_REVERT = (By.XPATH, "//div[@id='revertable']")
+
+    # Элемент Not Revert вкладки Revert Draggable
+    REVERT_DRAGGABLE_NOT_REVERT = (By.XPATH, "//div[@id='notRevertable']")
+
+    # Элемент Drop box вкладки Revert Draggable
+    REVERT_DRAGGABLE_DROP_BOX = (By.XPATH, "//div[@class='revertable-drop-container']/div[2]")
+
+    # Текст элемент Drop box вкладки Revert Draggable
+    REVERT_DRAGGABLE_DROP_BOX_TEXT = (By.XPATH, "//div[@class='revertable-drop-container']/div[2]/p")
 
 class DroppablePage(MainPage):
 
@@ -68,3 +100,47 @@ class DroppablePage(MainPage):
         print(text_before)
         print(text_after)
         return text_before, text_after
+
+    def prevent_propogation_tab(self):
+        """Перемещает элемент Drag me в inner и outer боксы"""
+        self.find_element(DroppablePageLocators.PREVENT_PROPOGATION_TAB).click()
+        drag_me = self.find_element(DroppablePageLocators.PREVENT_PROPOGATION_DRAG_ME)
+        inner_not_greedy_drop_box = self.find_element(DroppablePageLocators.NOT_GREEDY_INNER_DROP_BOX)
+        inner_greedy_drop_box = self.find_element(DroppablePageLocators.GREEDY_INNER_DROP_BOX)
+        outer_greedy_drop_box = self.find_element(DroppablePageLocators.GREEDY_OUTER_DROP_BOX)
+        outer_not_greedy_text_before = self.find_element(DroppablePageLocators.NOT_GREEDY_OUTER_DROP_BOX).text
+        inner_not_greedy_text_before = self.find_element(DroppablePageLocators.NOT_GREEDY_INNER_DROP_BOX).text
+        self.drag_and_drop_elements(drag_me, inner_not_greedy_drop_box)
+        outer_not_greedy_text_after = self.find_element(DroppablePageLocators.NOT_GREEDY_OUTER_DROP_BOX).text
+        inner_not_greedy_text_after = self.find_element(DroppablePageLocators.NOT_GREEDY_INNER_DROP_BOX).text
+        inner_greedy_text_before = self.find_element(DroppablePageLocators.GREEDY_INNER_DROP_BOX).text
+        self.drag_and_drop_elements(drag_me, inner_greedy_drop_box)
+        inner_greedy_text_after = self.find_element(DroppablePageLocators.GREEDY_INNER_DROP_BOX).text
+        outer_greedy_text_before = self.find_element(DroppablePageLocators.GREEDY_OUTER_DROP_BOX).text
+        self.drag_and_drop_elements(drag_me, outer_greedy_drop_box)
+        outer_greedy_text_after = self.find_element(DroppablePageLocators.GREEDY_OUTER_DROP_BOX).text
+        print(outer_not_greedy_text_before, inner_not_greedy_text_before)
+        print(outer_not_greedy_text_after, inner_not_greedy_text_after)
+        print(inner_greedy_text_before, inner_greedy_text_after)
+        print(outer_greedy_text_before, outer_greedy_text_after)
+        return outer_not_greedy_text_before, inner_not_greedy_text_before, outer_not_greedy_text_after, inner_not_greedy_text_after, inner_greedy_text_before, inner_greedy_text_after, outer_greedy_text_before, outer_greedy_text_after
+
+    def revert_draggable_tab(self):
+        """Перемещает элементы Will Revert и Not Revert в drop box"""
+        self.find_element(DroppablePageLocators.REVERT_DRAGGABLE_TAB).click()
+        will_revert = self.find_element(DroppablePageLocators.REVERT_DRAGGABLE_WILL_REVERT)
+        not_revert = self.find_element(DroppablePageLocators.REVERT_DRAGGABLE_NOT_REVERT)
+        drop_box = self.find_element(DroppablePageLocators.REVERT_DRAGGABLE_DROP_BOX)
+        text_before = self.find_element(DroppablePageLocators.REVERT_DRAGGABLE_DROP_BOX_TEXT).text
+        self.drag_and_drop_elements(not_revert, drop_box)
+        text_after = self.find_element(DroppablePageLocators.REVERT_DRAGGABLE_DROP_BOX_TEXT).text
+        not_revert_position_before = self.find_element(DroppablePageLocators.REVERT_DRAGGABLE_NOT_REVERT).get_attribute('style')
+        self.drag_and_drop_elements(not_revert, will_revert)
+        not_revert_position_after = self.find_element(DroppablePageLocators.REVERT_DRAGGABLE_NOT_REVERT).get_attribute('style')
+        will_revert_position_before = self.find_element(DroppablePageLocators.REVERT_DRAGGABLE_WILL_REVERT).get_attribute('style')
+        self.drag_and_drop_elements(will_revert, drop_box)
+        will_revert_position_after = self.find_element(DroppablePageLocators.REVERT_DRAGGABLE_WILL_REVERT).get_attribute('style')
+        print(text_before, text_after)
+        print(not_revert_position_before, not_revert_position_after)
+        print(will_revert_position_before, will_revert_position_after)
+        return text_before, text_after, not_revert_position_before, not_revert_position_after, will_revert_position_before, will_revert_position_after
--- tests/test_droppable_page.py
+++ tests/test_droppable_page.py
@@ -13,4 +13,13 @@ def test_droppable(browser):
     assert simple_text_before != simple_text_after, "Перемещение элемента Drag me в элемент Drop box не прошло"
     accept_text_before, accept_text_after = test_droppable.accept_tab()
     assert accept_text_before == 'Drop here', "Перемещение элементов Not Acceptable не прошло"
-    assert accept_text_after == 'Dropped', "Перемещение элементов Acceptable не прошло"
+    assert accept_text_after == 'Dropped!', "Перемещение элементов Acceptable не прошло"
+    outer_not_greedy_text_before, inner_not_greedy_text_before, outer_not_greedy_text_after, inner_not_greedy_text_after, inner_greedy_text_before, inner_greedy_text_after, outer_greedy_text_before, outer_greedy_text_after = test_droppable.prevent_propogation_tab()
+    assert outer_not_greedy_text_before != outer_not_greedy_text_after, "Перемещение элемента Drag me в элемент Drop box не прошло"
+    assert inner_not_greedy_text_before != inner_not_greedy_text_after, "Перемещение элемента Drag me в элемент Drop box не прошло"
+    assert inner_greedy_text_before != inner_greedy_text_after, "Перемещение элемента Drag me в элемент Drop box не прошло"
+    assert outer_greedy_text_before != outer_greedy_text_after, "Перемещение элемента Drag me в элемент Drop box не прошло"
+    text_before, text_after, not_revert_position_before, not_revert_position_after, will_revert_position_before, will_revert_position_after = test_droppable.revert_draggable_tab()
+    assert text_before != text_after, "Перемещение элемента Not Revert в элемент Drop box не прошло"
+    assert not_revert_position_before != not_revert_position_after, "Элемент Not Revert не вернулся в Drop Box"
+    assert will_revert_position_before != will_revert_position_after, "Элемент Will Revert не вернулся в исходное положение"
